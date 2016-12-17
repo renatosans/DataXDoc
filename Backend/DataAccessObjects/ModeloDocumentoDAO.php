6751e98f9ccdc9ccc6cb34ccdbf94af46e27dd22
renatosans <renato@datacopy.com.br>
2016-12-17 15:19:06 -0200

    Alterações no projeto

--- Backend/DataAccessObjects/ModeloDocumentoDAO.php
+++ Backend/DataAccessObjects/ModeloDocumentoDAO.php
@@ -2,8 +2,29 @@
 
 class ModeloDocumentoDAO{
 
-    function ModeloDocumentoDAO(){
+    var $mysqlConnection;
+    var $showErrors;
 
+    #construtor
+    function ModeloDocumentoDAO($mysqlConnection){
+        $this->mysqlConnection = $mysqlConnection;
+        $this->showErrors = 0;
+    }
+
+    function StoreRecord($dto){
+        return null;
+    }
+
+    function DeleteRecord($id){
+        return null;
+    }
+
+    function RetrieveRecord($id){
+        return null;
+    }
+
+    function RetrieveRecordArray($filter = null){
+        return null;
     }
 
 }
