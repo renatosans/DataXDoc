a82ae365b7c18a4d1f07ebbf04ff8d04adb7aa66
renatosans <renato@datacopy.com.br>
2016-12-17 19:52:23 -0200

    Inclusão de arquivo

---
+++ Backend/defines.php
@@ -0,0 +1,26 @@
+<?php
+
+    // Tipos de banco de dados suportados no sistema
+    $databaseType = Array(
+        "mySql",
+        "sqlServer",
+        "both"
+    );
+
+    // Parâmetros de login no banco de dados
+    $mysqlAuthentication = Array(
+        "host"=>"localhost",
+        "username"=>"root",
+        "password"=>"P@ssw0rd",
+        "database"=>"dataxdoc"
+    );
+
+    // Parâmetros de login no banco de dados
+    $sqlserverAuthentication = Array(
+        "host"=>"localhost",
+        "username"=>"sapBusinessOne",
+        "password"=>"P@ssw0rd",
+        "database"=>"SBO_DATACOPY"
+    );
+
+?>
