c1cbf011e780d2855bae010c8d6f4d56edd5cec1
renatosans <renato@datacopy.com.br>
2016-12-17 17:13:13 -0200

    Alterações no projeto

--- Backend/DataAccessObjects/ModeloDocumentoDAO.php
+++ Backend/DataAccessObjects/ModeloDocumentoDAO.php
@@ -12,6 +12,22 @@ class ModeloDocumentoDAO{
     }
 
     function StoreRecord($dto){
+        // Monta a query dependendo do id como INSERT ou UPDATE
+        $query = "INSERT INTO modelodocumento VALUES (NULL, '".$dto->nome."', '".$dto->descricao."');";
+        if ($dto->id > 0)
+            $query = "UPDATE modelodocumento SET nome = '".$dto->nome."', descricao = '".$dto->descricao."' WHERE id = ".$dto->id.";";
+
+        $result = mysql_query($query, $this->mysqlConnection);
+        if ($result) {
+            $insertId = mysql_insert_id($this->mysqlConnection);
+            if ($insertId == null) return $dto->id;
+            return $insertId;
+        }
+
+        if ((!$result) && ($this->showErrors)) {
+            print_r(mysql_error());
+            echo '<br/>';
+        }
         return null;
     }
 
