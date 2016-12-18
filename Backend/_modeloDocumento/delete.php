2df54f05d2796f9c36ed42a28c1248c4f2f4aab7
renatosans <renato@datacopy.com.br>
2016-12-18 14:30:29 -0200

    Alterações no projeto

---
+++ Backend/_modeloDocumento/delete.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once("../defines.php");
+include_once("../DataConnector.php");
+include_once("../DataAccessObjects/ModeloDocumentoDAO.php");
+include_once("../DataTransferObjects/ModeloDocumentoDTO.php");
+
+// Abre a conexao com o banco de dados
+$dataConnector = new DataConnector('mySql');
+$dataConnector->OpenConnection();
+if ($dataConnector->mysqlConnection == null) {
+    echo 'Não foi possível se connectar ao bando de dados!';
+    exit;  
+}
+
+// Cria o objeto de mapeamento objeto-relacional
+$modeloDocumentoDAO = new ModeloDocumentoDAO($dataConnector->mysqlConnection);
+$modeloDocumentoDAO->showErrors = 1;
+
+if( !isset($_POST['reg']) ){
+    echo "Selecione os registros que deseja excluir";
+    exit;
+}
+
+foreach($_POST['reg'] as $key=>$reg){
+    if( !$counterDAO->DeleteRecord($reg) ){
+        echo "Não foi possivel efetuar a operação...";
+        exit;
+    }
+}
+
+echo "Operação efetuada com sucesso!";
+
+// Fecha a conexão com o banco de dados
+$dataConnector->CloseConnection();
+
+?>
