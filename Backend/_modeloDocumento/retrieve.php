2ddd42fdf596251a1fb0ea61deff7b611b2f077b
renatosans <renato@datacopy.com.br>
2016-12-18 15:58:17 -0200

    Alteraçoes no projeto

---
+++ Backend/_modeloDocumento/retrieve.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once("../defines.php");
+include_once("../DataConnector.php");
+include_once("../DataAccessObjects/ModeloDocumentoDAO.php");
+include_once("../DataTransferObjects/ModeloDocumentoDTO.php");
+
+// Abre a conexao com o banco de dados
+$dataConnector = new DataConnector('mySql');
+$dataConnector->OpenConnection();
+if ($dataConnector->mysqlConnection == null) {
+    echo 'Não foi possível se connectar ao bando de dados!';
+    exit;  
+}
+
+// Cria o objeto de mapeamento objeto-relacional
+$modeloDocumentoDAO = new ModeloDocumentoDAO($dataConnector->mysqlConnection);
+$modeloDocumentoDAO->showErrors = 1;
+
+/* recupera a lista de registros em formato JSON */
+
+// Fecha a conexão com o banco de dados
+$dataConnector->CloseConnection();
+
+?>
