27ca95f749e6e15170dc3bb03899ebdc90fe013a
renatosans <renato@datacopy.com.br>
2016-12-19 12:06:12 -0200

    Alterações no projeto

--- Backend/_campoDocumento/retrieve.php
+++ Backend/_campoDocumento/retrieve.php
@@ -17,6 +17,17 @@ if ($dataConnector->mysqlConnection == null) {
 $campoDocumentoDAO = new CampoDocumentoDAO($dataConnector->mysqlConnection);
 $campoDocumentoDAO->showErrors = 1;
 
+/* recupera a lista de registros em formato JSON */
+$id = 0;
+if ( isset($_POST["reg"]) && ($_POST["reg"] != 0) ) {
+    $id = $_POST['reg'];
+    $campoDocumentoDTO = $campoDocumentoDAO->RetrieveRecord($id);
+    echo json_encode($campoDocumentoDTO);
+}
+if ($id == 0){ // nenhum registro especificado, retorna todos
+    $lista = $campoDocumentoDAO->RetrieveRecordArray(null);
+    echo json_encode($lista);
+}
 
 // Fecha a conexão com o banco de dados
 $dataConnector->CloseConnection();
