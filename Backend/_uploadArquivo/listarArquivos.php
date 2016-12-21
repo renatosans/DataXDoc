9702f3c2375233aea70be0853e6e5b9703985fc5
renatosans <renato@datacopy.com.br>
2016-12-21 18:23:11 -0200

    Correção no upload

--- Backend/_uploadArquivo/listarArquivos.php
+++ Backend/_uploadArquivo/listarArquivos.php
@@ -3,7 +3,11 @@
     include_once("../defines.php");
 
     // Lista os arquivos do diretório temporário e retorna uma lista em formato JSON
-    $fileArray = scandir("../../".$tempDir); // volta dois diretórios na árvore de diretórios
+    $fileArray = array();
+    foreach (new DirectoryIterator("../../".$tempDir) as $fileInfo) {
+        if($fileInfo->isDir()) continue;
+        array_push($fileArray, $fileInfo->getFilename());
+    }
     echo json_encode($fileArray);
 
 ?>
