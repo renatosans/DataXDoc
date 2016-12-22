a94d6828d4cb9066a69879fe9596a15b35e2980c
renatosans <renato@datacopy.com.br>
2016-12-22 16:06:22 -0200

    Alterações na solução

---
+++ Backend/DataTransferObjects/DocumentoDTO.php
@@ -0,0 +1,16 @@
+<?php
+
+class DocumentoDTO{
+    var $id               = 0;
+    var $repositorio      = 0;
+    var $nome             = "";
+	var $arquivo          = null;
+
+
+    function DocumentoDTO(){
+
+    }
+
+}
+
+?>
