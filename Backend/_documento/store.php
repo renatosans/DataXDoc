f2fd0b9564677cfbc23775808a77e606da9f5e04
renatosans <renato@datacopy.com.br>
2016-12-24 10:03:04 -0200

    Alterações na solução

--- Backend/_documento/store.php
+++ Backend/_documento/store.php
@@ -13,9 +13,26 @@ if ($dataConnector->mysqlConnection == null) {
     exit;  
 }
 
+// Cria o objeto de mapeamento objeto-relacional
+$documentoDAO = new DocumentoDAO($dataConnector->mysqlConnection);
+$documentoDAO->showErrors = 1;
 
+$id = 0;
+$documento = new DocumentoDTO();
+if ( isset($_REQUEST["reg"]) && ($_REQUEST["reg"] != 0) ) {
+    $id = $_REQUEST["reg"];
+    $documento = $documentoDAO->RetrieveRecord($id);
+}
+$documento->repositorio = $_REQUEST["repositorio"];
+$documento->nome        = $_REQUEST["nome"];
+$documento->arquivo     = $_REQUEST["arquivo"];
 
-
+$recordId = $documentoDAO->StoreRecord($documento);
+if ($recordId == null) {
+    echo "Não foi possivel efetuar a operação...";
+    exit;
+}
+echo $recordId; // retorna o id do registro inserido para a página de edição
 
 // Fecha a conexão com o banco de dados
 $dataConnector->CloseConnection();
