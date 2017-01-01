9d66105d2eeba2ca8fba10716acccc8b537202b2
renatosans <renato@datacopy.com.br>
2017-01-01 08:16:10 -0200

    Alterações no projeto

--- Backend/defines.php
+++ Backend/defines.php
@@ -11,7 +11,7 @@
     $mysqlAuthentication = Array(
         "host"=>"localhost",
         "username"=>"root",
-        "password"=>"",
+        "password"=>"P@ssw0rd",
         "database"=>"dataxdoc"
     );
 
