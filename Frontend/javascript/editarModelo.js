56751f3c1b7807d33068f0be1b1ed95a74746823
renatosans <renato@datacopy.com.br>
2017-01-02 13:47:36 -0200

    Alterações no projeto

--- Frontend/javascript/editarModelo.js
+++ Frontend/javascript/editarModelo.js
@@ -24,5 +24,32 @@ function NovoCampo(){
 function ListarCampos(){
     var targetUrl = "../Backend/_campoDocumento/retrieve.php";
     var callParameters = {'modeloDocumento' : recordId}; // filtra os campos por modelo de documento
-    $.ajax({ type: 'POST', url: targetUrl, data: callParameters, success: function(response) { ParseResponse(response); }, async: false });
+    $.ajax({ type: 'POST', url: targetUrl, data: callParameters, success: function(response) {
+        var lista = document.getElementById("lista");
+        lista.innerHTML = '<tr class="info"> <td>Nome do campo</td><td>Formato</td><td>Ações</td> </tr>'; // Limpa a lista antes de recarregar
+
+        var itemList = JSON.parse(response);
+        for(var item in itemList) {
+            ListarLinha(itemList[item]);
+        }
+    }
+    });
+}
+
+var fieldTypes = ["NONE","NÚMERO","TEXTO","DATA"];
+
+function ListarLinha(campoDocumento){
+    var row = document.createElement('tr');
+    var col1 = document.createElement('td');
+    col1.innerText = campoDocumento.nome;
+    var col2= document.createElement('td');
+    col2.innerText = fieldTypes[campoDocumento.tipo];
+    var col3 = document.createElement('td');
+    // var editClick = 'EditarItem(' + campoDocumento.id + ');';
+    var deleteClick = 'ExcluirItem(' + campoDocumento.id + '); ListarItems(9);'; // retirar esse 9 hardcoded posteriormente
+    col3.innerHTML = '&nbsp; <img src="images/edit.png" /> &nbsp; <img src="images/dump.png" onclick="' + deleteClick + '" />';
+    row.appendChild(col1);
+    row.appendChild(col2);
+    row.appendChild(col3);
+    lista.appendChild(row);
 }
