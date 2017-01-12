614d6b8401f80191953ef7e6672e7285ebb63d57
renatosans <renato@datacopy.com.br>
2017-01-12 18:26:02 -0200

    Ajustes

--- Backend/_uploadArquivo/upload.php
+++ Backend/_uploadArquivo/upload.php
@@ -1,8 +1,7 @@
 <?php
 
 if (isset($_FILES['file']) && !empty($_FILES['file']['name'])) {
-  file_put_contents('uploads/post.txt', 'name=' . $_POST['name'] . ',count=' . $_POST['i'] . PHP_EOL, FILE_APPEND);
-  move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name']);
+  move_uploaded_file($_FILES['file']['tmp_name'], "_tempDir/" . $_FILES['file']['name']);
 
   $ret = array('status' => 'ok');
 } else {
@@ -12,3 +11,5 @@ if (isset($_FILES['file']) && !empty($_FILES['file']['name'])) {
 header('Content-Type: application/json');
 echo json_encode($ret);
 exit;
+
+?>
