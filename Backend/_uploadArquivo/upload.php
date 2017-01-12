2046a4e5c361c7e2345a46fc729401201c67fd0f
renatosans <renato@datacopy.com.br>
2017-01-12 17:23:14 -0200

    Alterações no projeto

--- Backend/_uploadArquivo/upload.php
+++ Backend/_uploadArquivo/upload.php
@@ -1,62 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <title>Upload ajax</title>
-</head>
-<body>
+<?php
 
-<form action="upload.php" method="post" id="upload">
-  <input type="file" name="file" id="file" accept="image/*" />
-  <input type="text" name="name" value="complemento" />
-  <input type="submit" value="Send!" />
-</form>
+if (isset($_FILES['file']) && !empty($_FILES['file']['name'])) {
+  file_put_contents('uploads/post.txt', 'name=' . $_POST['name'] . ',count=' . $_POST['i'] . PHP_EOL, FILE_APPEND);
+  move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name']);
 
-<div id="preview"></div>
+  $ret = array('status' => 'ok');
+} else {
+  $ret = array('error' => 'no_file');
+}
 
-<script>
-var $formUpload = document.getElementById('upload'),
-    $preview = document.getElementById('preview'),
-    i = 0;
-
-$formUpload.addEventListener('submit', function(event){
-  event.preventDefault();
-
-  var xhr = new XMLHttpRequest();
-
-  xhr.open("POST", $formUpload.getAttribute('action'));
-
-  var formData = new FormData($formUpload);
-  formData.append("i", i++);
-  xhr.send(formData);
-
-  xhr.addEventListener('readystatechange', function() {
-    if (xhr.readyState === 4 && xhr.status == 200) {
-      var json = JSON.parse(xhr.responseText);
-
-      if (!json.error && json.status === 'ok') {
-        $preview.innerHTML += '<br />Enviado!!';
-      } else {
-        $preview.innerHTML = 'Arquivo não enviado';
-      }
-
-    }
-  });
-
-  xhr.upload.addEventListener("progress", function(e) {
-    if (e.lengthComputable) {
-      var percentage = Math.round((e.loaded * 100) / e.total);
-      $preview.innerHTML = String(percentage) + '%';
-    }
-  }, false);
-
-  xhr.upload.addEventListener("load", function(e){
-    $preview.innerHTML = String(100) + '%';
-  }, false);
-
-}, false);
-
-</script>
-
-</body>
-</html>
+header('Content-Type: application/json');
+echo json_encode($ret);
+exit;
